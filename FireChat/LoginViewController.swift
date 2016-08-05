6dbf4fab0dbd5ab6fb6a003fc130d482770894f3
bassaer <rf.1799@gmail.com>
2016-08-06 00:00:02 +0900

    Finish base chat function

---
+++ FireChat/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  FireChat
+//
+//  Created by Nakayama on 2016/08/05.
+//  Copyright © 2016年 Nakayama. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+}
